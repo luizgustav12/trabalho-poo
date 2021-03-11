4f1926f38353bfa0c68ec94065be84854f75bd04
Luiz Gustavo <gustav_ops@hotmail.com>
2021-03-11 20:38:48 -0300

    Exemplo teste

--- NewProject/src/main/java/Lalaland/Lalaland.java
+++ NewProject/src/main/java/Lalaland/Lalaland.java
@@ -4,6 +4,6 @@ package Lalaland;
 
 public class Lalaland {
     public static void main(String[] args) {
-        System.out.println("Hello mundo");
+        System.out.println("Hello mundo!!!");
     }
 }
